0b12c78cde2576a34eb58df6e696ffe118745b17
granis <granis@gmail.com>
2021-07-20 10:52:05 +0200

    del 2

---
+++ Del2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Del2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv din ålder");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+
+            Console.WriteLine("Din ålder är " + age);
+
+            int birthYear = 2021 - age;
+            Console.WriteLine("Du föddes år " + birthYear);
+        }
+    }
+}

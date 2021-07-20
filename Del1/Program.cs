47d8378d9d4377d32000bc378de493968241433d
granis <granis@gmail.com>
2021-07-20 10:46:27 +0200

    del 1

---
+++ Del1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Del1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt namn och tryck på enter");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hej " + name);
+        }
+    }
+}

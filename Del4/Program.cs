58b7f5e05636067c203b748647b5d9fb8483530a
granis <granis@gmail.com>
2021-07-20 11:11:07 +0200

    del 4

---
+++ Del4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Del4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*while (true)
+            {
+                Console.WriteLine("Skriv in ditt namn eller exit för att avsluta");
+                string name = Console.ReadLine();
+
+                if(name.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Längden på ditt namn är " + name.Length);
+            }*/
+
+            for (int counter=0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter+1);
+            }
+        }
+    }
+}

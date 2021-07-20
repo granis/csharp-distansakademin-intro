d16a48e4f846caaa5c211ac5fb899df200ed73e1
granis <granis@gmail.com>
2021-07-20 11:17:12 +0200

    del 5

---
+++ Del5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Del5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create random generator
+            Random rnd = new Random();
+
+            //iterate 20 times
+            for (int i = 0; i < 20; i++)
+            {
+                //get and print a random number
+                int myRandomNumber = rnd.Next(1, 10+1); //add 1 so max-value is included
+                Console.WriteLine(myRandomNumber);
+            }
+        }
+    }
+}

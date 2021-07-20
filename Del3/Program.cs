da9095211f056ecf72f0adc59bbbbc2804b7fffc
granis <granis@gmail.com>
2021-07-20 10:59:00 +0200

    Del 3

---
+++ Del3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Del3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv din ålder");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+
+            Console.WriteLine("Din ålder är " + input);
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Du får ta körkort");
+            }
+            else if (age >= 16)
+            {
+                Console.WriteLine("Du får övningsköra bil");
+            }
+            else
+            {
+                Console.WriteLine("Du får köra sparkcykel");
+            }
+        }
+    }
+}

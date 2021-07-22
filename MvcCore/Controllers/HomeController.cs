ceedefc37b3d6cab0152330d59872cf323da1a62
granis <granis@gmail.com>
2021-07-22 11:26:09 +0200

    add a test page(view)

--- MvcCore/Controllers/HomeController.cs
+++ MvcCore/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MvcCore.Controllers
             return View();
         }
 
+        public IActionResult TestPage()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
